999cdbf567ace5dd2ba04c5acd7cb8e759eb4a59
Shafaaf Hossain <EL-CAPITAN-2016@Mackintoshs-MacBook-Pro-3.local>
2021-05-10 23:55:46 -0400

    adfaf

---
+++ react-client/README.md
@@ -0,0 +1,7 @@
+## Social Media App React client
+
+Tutorial: https://www.youtube.com/playlist?list=PLMhAeHCz8S38ryyeMiBPPUnFAiWnoPvWP
+
+Code: https://github.com/hidjou/classsed-react-firebase-client
+
+TODO: Start of: #14 Getting Started With React: 10:40
---
+++ react-client/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Router>
+                    <Switch>
+                        <Route exact path = '/' component={home}/>
+                        <Route exact path = '/login' component={login}/>
+                        <Route exact path = '/signup' component={signup}/>
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
+}
+
+export default App;
