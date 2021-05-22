55c23a684caf42a677579a4f2718447efed5d76a
Shafaaf Hossain <EL-CAPITAN-2016@Mackintoshs-MacBook-Pro-3.local>
2021-05-22 02:21:56 -0400

    bshkfbashk

--- react-client/src/components/ profile.js
+++ react-client/src/components/ profile.js
@@ -2,14 +2,40 @@ import React, {Component} from 'react';
 import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
 import {connect} from "react-redux";
+import CardContent from "@material-ui/core/CardContent";
+import {Card, CardActionArea, CardActions} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import CardMedia from "@material-ui/core/CardMedia";
+
+import CalendarToday from '@material-ui/icons/CalendarToday';
+import LinkIcon from '@material-ui/icons/Link';
+import LocationOn from '@material-ui/icons/LocationOn';
+
+import dayjs from "dayjs";
+
+
 
 const styles = {
-
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 140,
+    },
+    cardActionArea: {
+        textAlign: 'center'
+    },
+    link: {
+        color: 'inherit',
+        textDecoration: 'none'
+    }
 };
 
 class Profile extends Component {
 
     render() {
+        const {classes} = this.props;
         const {user} = this.props;
         const {ui} = this.props;
 
@@ -32,14 +58,61 @@ class Profile extends Component {
                 return <p>Loading</p>;
             } else {
                 return (
-                    <div>
-                        <img src={user.credentials.profilePicUrl} alt="profilePicUrl"/>
-                        <p>Email: {user.credentials.email}</p>
-                        <p>User handle: {user.credentials.handle}</p>
-                        {user.credentials.bio? (<p>Bio: {user.credentials.bio}</p>) : null}
-                        {user.credentials.website? (<p>Website: {user.credentials.website}</p>) : null}
-                        {user.credentials.location? (<p>Location: {user.credentials.location}</p>) : null}
-                    </div>
+                    <Card className={classes.root}>
+                        <CardActionArea className={classes.cardActionArea}>
+                            <CardMedia
+                                className={classes.media}
+                                image={user.credentials.profilePicUrl}
+                                title="Profile Picture"
+                            />
+                            <CardContent>
+                                {
+                                    user.credentials.handle &&
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        @{user.credentials.handle}
+                                    </Typography>
+                                }
+                                {
+                                    user.credentials.bio &&
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        {user.credentials.bio}
+                                    </Typography>
+                                }
+                                <br/>
+                                {
+                                    user.credentials.location &&
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        <LocationOn fontSize = 'inherit' color="primary" /> {user.credentials.location}
+                                    </Typography>
+                                }
+                                {
+                                    user.credentials.website &&
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        <LinkIcon fontSize = 'inherit' color="primary" />
+                                        <a className={classes.link} href={user.credentials.website} target="_blank" rel="noopener noreferrer">
+                                            {' '}
+                                            {user.credentials.website}
+                                        </a>
+                                    </Typography>
+                                }
+                                {
+                                    user.credentials.createdAt &&
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        <CalendarToday fontSize="inherit" color="primary" />{' '}
+                                        Joined {dayjs(user.credentials.createdAt).format('MMM YYYY')}
+                                    </Typography>
+                                }
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
+                            </Button>
+                            <Button size="small" color="primary">
+                                Learn More
+                            </Button>
+                        </CardActions>
+                    </Card>
                 );
             }
         } else {
