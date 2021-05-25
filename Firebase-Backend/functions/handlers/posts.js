f535e398cd18ee55cdd80601e1854834edba2ae4
Shafaaf Hossain <EL-CAPITAN-2016@Mackintoshs-MacBook-Pro-3.local>
2021-05-24 20:42:08 -0400

    sbdkjfbas

--- Firebase-Backend/functions/handlers/posts.js
+++ Firebase-Backend/functions/handlers/posts.js
@@ -21,6 +21,8 @@ exports.getAllPosts = (req, res) => {
                                     body: doc.data().body,
                                     userHandle: doc.data().userHandle,
                                     createdAt: doc.data().createdAt,
+                                    commentCount: doc.data().commentCount,
+                                    likeCount: doc.data().likeCount,
                                     profilePicUrl: profilePicUrl
                                 });
                                 resolve();
